3d7a59b7ed4efe271363e9a518a16958cc419dbe
Hansell Esteban Ramos Osorio <hansell.ramos@globant.com>
2016-11-30 18:49:47 -0500

    Added browserSync with watch task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
  */
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 gulp.task('default',['sass']);
 
@@ -10,8 +12,18 @@ gulp.task('sass',function(){
     return gulp.src('src/sass/app.scss')
         .pipe(sass())
         .pipe(gulp.dest('app/'))
+        .pipe(reload({stream:true}))
 });
 
 gulp.task('watch',function(){
     gulp.watch('src/sass/*.scss',['sass']);
+});
+
+gulp.task('serve',['sass'],function(){
+    browserSync({
+        server: {
+            baseDir: ['app']
+        }
+    });
+    gulp.start('watch');
 });
