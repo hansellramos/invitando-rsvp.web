8b2857b9e333896a754a2c573877b1b077260e02
Hansell Esteban Ramos Osorio <hansell.ramos@globant.com>
2016-12-01 11:45:40 -0500

    Added minified css build task

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,26 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default',['sass']);
 
 gulp.task('sass',function(){
     return gulp.src('src/sass/app.scss')
-        .pipe(sass())
+        .pipe(sourcemaps.init())
+        .pipe(sass()).on('error', sass.logError)
+        .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('app/'))
         .pipe(reload({stream:true}))
+});
+
+gulp.task('sass:prod',function(){
+    return gulp.src('src/sass/app.scss')
+        .pipe(sass({ outputStyle: 'compressed'})).on('error', sass.logError)
+        .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
+        .pipe(gulp.dest('app/dist/'))
 });
 
 gulp.task('watch',function(){
@@ -27,3 +39,5 @@ gulp.task('serve',['sass'],function(){
     });
     gulp.start('watch');
 });
+
+gulp.task('build', ['sass:prod']);
